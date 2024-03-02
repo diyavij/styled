2887c07e7b70c4caf70cc2c3dbe8cbdf980ccbd2
Diya Vij <vij.diya@gmail.com>
2024-03-02 12:58:06 -0800

    add epoch to gan

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import tensorflow as tf
 from tensorflow.keras import layers, models, datasets, optimizers
 from PIL import Image
 
-# Load and preprocess dataset
 def load_dataset(data_dir):
     image_paths = []
     for root, dirs, files in os.walk(data_dir):
@@ -65,7 +64,7 @@ epochs = 50
 batch_size = 128
 
 # Load dataset
-data_dir = "C:/Users/vijdi/OneDrive/Desktop/CSProjects/athenahacks24/fashion_data"
+data_dir = "C:/Users/vijdi/OneDrive/Desktop/CSProjects/athenahacks24/fashion_data/images"
 x_train = load_dataset(data_dir)
 
 # Reshape and add channel dimension
@@ -77,8 +76,59 @@ discriminator = build_discriminator()
 gan = build_gan(generator, discriminator)
 gan.compile(loss='binary_crossentropy', optimizer=optimizers.Adam(lr=0.0002, beta_1=0.5))
 
+# Define hyperparameters
+latent_dim = 100
+epochs = 50
+batch_size = 128
+save_interval = 10  # Save generated images every 10 epochs
+
+# Function to save generated images
+def save_generated_images(generator, epoch, examples=100, dim=(10, 10), figsize=(10, 10)):
+    noise = np.random.normal(0, 1, (examples, latent_dim))
+    generated_images = generator.predict(noise)
+
+    # Rescale images 0 - 1
+    generated_images = 0.5 * generated_images + 0.5
+
+    fig, axs = plt.subplots(dim[0], dim[1], figsize=figsize, sharex=True, sharey=True)
+    cnt = 0
+    for i in range(dim[0]):
+        for j in range(dim[1]):
+            axs[i, j].imshow(generated_images[cnt, :, :, 0], cmap='gray')
+            axs[i, j].axis('off')
+            cnt += 1
+    fig.savefig(f"generated_images_epoch_{epoch}.png")
+    plt.close()
+
 # Training loop
 for epoch in range(epochs):
     np.random.shuffle(x_train)
     for batch in range(x_train.shape[0] // batch_size):
-        # Train
+        # Train Discriminator
+        noise = np.random.normal(0, 1, (batch_size, latent_dim))
+        fake_images = generator.predict(noise)
+
+        real_images = x_train[batch * batch_size: (batch + 1) * batch_size]
+
+        # Labels for real and fake images
+        real_labels = np.ones((batch_size, 1))
+        fake_labels = np.zeros((batch_size, 1))
+
+        # Train the discriminator
+        d_loss_real = discriminator.train_on_batch(real_images, real_labels)
+        d_loss_fake = discriminator.train_on_batch(fake_images, fake_labels)
+        d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+
+        # Train Generator via GAN
+        noise = np.random.normal(0, 1, (batch_size, latent_dim))
+        misleading_targets = np.ones((batch_size, 1))
+
+        # Update the generator (via the GAN model)
+        g_loss = gan.train_on_batch(noise, misleading_targets)
+
+    # Print progress
+    print(f"Epoch {epoch+1}/{epochs}, Discriminator Loss: {d_loss}, Generator Loss: {g_loss}")
+
+    # Save generated images at specified intervals
+    if epoch % save_interval == 0:
+        save_generated_images(generator, epoch)
